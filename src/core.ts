d74b9d33cc46719c7229f0a97a398384ae6c7f8c
hocgin <hocgin@gmail.com>
2022-09-20 00:19:44 +0800

    upload

--- src/core.ts
+++ src/core.ts
@@ -1,16 +1,29 @@
 import {Inputs, Outputs} from "./main";
 import {upload, preview} from "./uploadapi";
+import * as exec from "@actions/exec";
 
 export async function run(input: Inputs): Promise<Outputs> {
-    let preview_qrcode;
-    if (input.action_type === 'upload') {
-        await upload(input);
-    } else if (input.action_type === 'preview') {
-        preview_qrcode = await preview(input);
-    } else {
-        throw new Error(`unSupport action_type ${input.action_type}`);
-    }
+    const options: any = {};
+    options.listeners = {
+        stdout: (data: Buffer) => {
+            console.log('stdout', data.toString());
+        },
+        stderr: (data: Buffer) => {
+            console.log('stderr', data.toString());
+        }
+    };
+    await exec.exec('mp-ci', ['-h'], options);
+
+
+    // let preview_qrcode;
+    // if (input.action_type === 'upload') {
+    //     await upload(input);
+    // } else if (input.action_type === 'preview') {
+    //     preview_qrcode = await preview(input);
+    // } else {
+    //     throw new Error(`unSupport action_type ${input.action_type}`);
+    // }
     return {
-        preview_qrcode
+        // preview_qrcode
     };
 }
