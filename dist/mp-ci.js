f3a3dada1e0fe2cbced00bd98d40985e591dee35
hocgin <hocgin@gmail.com>
2022-09-20 00:43:00 +0800

    upload

---
+++ dist/mp-ci.js
@@ -0,0 +1,73 @@
+#!/usr/bin/env node
+const program = require('commander');
+const packageJson = require('../package.json');
+const Ci = require('../lib');
+
+program.version(packageJson.version).usage('[--options ...]');
+
+program
+  .command('upload [workspace]')
+  .option('--env [value]', '环境类型, 例如 dev|test|prod')
+  .option('--type [value]', '项目类型', 'miniProgram')
+  .option('--ver [value]', '发布版本号')
+  .option('--desc [value]', '发布简介')
+  .option('--pkp [value]', '私钥文件所在路径')
+  .option('--proxy [value]', '代理url')
+  .option('--robot [value]', '指定CI机器人，1 ~ 30', '1')
+  .description('上传代码')
+  .action(function(workspace, cmdObj) {
+    new Ci({
+      workspace: workspace || process.cwd(),
+      env: cmdObj.env,
+      version: cmdObj.ver,
+      desc: cmdObj.desc,
+      type: cmdObj.type,
+      pkp: cmdObj.pkp,
+      robot: cmdObj.robot,
+    }).upload({
+      proxy: cmdObj.proxy,
+    });
+  });
+
+program
+  .command('preview [workspace]')
+  .option('--env [value]', '环境类型, 例如 dev|test|prod')
+  .option('--type [value]', '项目类型', 'miniProgram')
+  .option('--ver [value]', '发布版本号')
+  .option('--desc [value]', '发布简介')
+  .option('--pkp [value]', '私钥文件所在路径')
+  .option('--qr [value]', '二维码文件的格式: terminal|base64|image', 'image')
+  .requiredOption('--qrDest [value]', '二维码文件保存路径 ', 'preview.png')
+  .option('--pagePath [value]', '预览页面路径')
+  .option('--searchQuery [value]', '预览页面路径启动参数，这里的&字符在命令行中应写成转义字符\\&')
+  .option('--proxy [value]', '代理url')
+  .option('--robot [value]', '指定CI机器人，1 ~ 30', '1')
+  .description('预览代码')
+  .action(function(workspace, cmdObj) {
+    new Ci({
+      workspace: workspace || process.cwd(),
+      env: cmdObj.env,
+      version: cmdObj.ver,
+      desc: cmdObj.desc,
+      type: cmdObj.type,
+      pkp: cmdObj.pkp,
+      qr: cmdObj.qr,
+      qrDest: cmdObj.qrDest,
+      robot: cmdObj.robot,
+    }).preview({
+      qr: cmdObj.qr,
+      qrDest: cmdObj.qrDest,
+      pagePath: cmdObj.pagePath,
+      searchQuery: cmdObj.searchQuery,
+      proxy: cmdObj.proxy,
+    });
+  });
+
+program.parse(process.argv);
+
+program.on('--help', () => {
+  console.log('  Examples:');
+  console.log('');
+  console.log('    $ mp-ci');
+  console.log('');
+});
