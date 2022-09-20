8c5641c4292a44db46487007bbe44fd35e29dcc0
hocgin <hocgin@gmail.com>
2022-09-20 20:40:12 +0800

    update

--- src/uploadcmd.ts
+++ src/uploadcmd.ts
@@ -7,20 +7,20 @@ import * as exec from "@actions/exec";
 export async function upload(input: Inputs, options: any) {
     let keyfile = toKeyFile(input.workspace, input.upload_key);
     let args = [
-        `--pkp ${keyfile}`,
-        `--type ${input.type}`
+        `--pkp=${keyfile}`,
+        `--type=${input.type}`
     ];
     if (input.env) {
-        args.push(`--env ${input.env}`);
+        args.push(`--env=${input.env}`);
     }
     if (input.version) {
-        args.push(`--ver ${input.version}`);
+        args.push(`--ver=${input.version}`);
     }
     if (input.description) {
-        args.push(`--desc ${input.description}`);
+        args.push(`--desc=${input.description}`);
     }
     if (input.proxy) {
-        args.push(`--proxy ${input.proxy}`);
+        args.push(`--proxy=${input.proxy}`);
     }
     await exec.exec('npx', ['mp-ci', 'upload', input.workspace, ...args], options);
 }
@@ -30,28 +30,28 @@ export async function preview(input: Inputs, options: any) {
     let pngfile = path.join(input.workspace, pngpath);
     let keyfile = toKeyFile(input.workspace, input.upload_key);
     let args = [
-        `--pkp ${keyfile}`,
-        `--qr base64`,
-        `--qrDest ${pngpath}`,
-        `--type ${input.type}`
+        `--pkp=${keyfile}`,
+        `--qr=base64`,
+        `--qrDest=${pngpath}`,
+        `--type=${input.type}`
     ];
     if (input.env) {
-        args.push(`--env ${input.env}`);
+        args.push(`--env=${input.env}`);
     }
     if (input.version) {
-        args.push(`--ver ${input.version}`);
+        args.push(`--ver=${input.version}`);
     }
     if (input.description) {
-        args.push(`--desc ${input.description}`);
+        args.push(`--desc=${input.description}`);
     }
     if (input.preview_pagepath) {
-        args.push(`--pagePath ${input.preview_pagepath}`);
+        args.push(`--pagePath=${input.preview_pagepath}`);
     }
     if (input.preview_pagequery) {
-        args.push(`--searchQuery ${input.preview_pagequery}`);
+        args.push(`--searchQuery=${input.preview_pagequery}`);
     }
     if (input.proxy) {
-        args.push(`--proxy ${input.proxy}`);
+        args.push(`--proxy=${input.proxy}`);
     }
 
     await exec.exec('npx', ['mp-ci', 'preview', input.workspace, ...args], options);
